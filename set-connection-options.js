3daa286ba2468e07e95d88058a1dce8e6c5a8029
Eugene Skopal <eugene.github-pagg@ae2fdesigns.com>
2018-03-01 07:47:35 -0500

    Options and messages working

--- set-connection-options.js
+++ set-connection-options.js
@@ -73,10 +73,16 @@ global.connectionOptions = {
         'acceptableLatencyMS': 'localThresholdMS'
     },
 
-    otherOptions: {
-        "allowAny": allowAny,
+    doNotSetOptions: ['ssl'],
+
+    ourOptions: {
+        "options": setOptions,      // Always look for this one first...
+
         "default": foundDefault,
-        "doDebugLog" : setDoDebugLog,
+        "allowAny": allowAny,
+        "doDebugLog": setDoDebugLog,
+        "filePath": setFilePath,
+        "file_path": setFilePath,
     },
 
     // Define a set of default options support SSL and X509 validation
@@ -106,17 +112,18 @@ global.connectionOptions = {
         'env:': readEnv
     },
 
-    envName: 'MONGO_CONNECTION_OPTIONS',
+    envPrefix: ['MONGO_SCO_', 'MONGO_CONNECTION_'],
 
     filePath: 'assets/app/',
 
     files: {},
 
-    envText: "",
+    mongoEnv: {},
 
     options: {},
 
     msgPrefix: 'set-connection-options --',
+    msgPaddng: '                         ',
 
     doDebugLog: true,
 
@@ -125,68 +132,80 @@ global.connectionOptions = {
     noop: function () {},
 };
 
-
 const self = global.connectionOptions;
 
 setDoDebugLog(self.doDebugLog);
 
-if (process.env[self.envName]) {
+const optionActions = {};           // create actions for each Option Name
+const ourOptionActions = {};
+const optionNames = {};       // Translate back from lower case to correct caseness
 
-    self.options = readOptions(self.envName);
+setupActions();         // Setup our action tables
 
-    checkOptions(self.options);
+loadEnvValues();
+
+processOurActions();        // Go process our actions
+processMongoEntries();      // Go process any other entries
+
+if (Object.keys(self.options)) {      // IF there is any work to do -- go do it...
 
     fixupOptions(self.options);
 
     Mongo.setConnectionOptions(self.options);
 }
 
-/**
- * Read the contents of the environmental variable and covert it into an object
- *   substitute the default values if requested or if there is an error
- * @param envName
- * @returns {*}
- */
-function readOptions(envName) {
-    self.envText = process.env[self.envName];
-    let text = self.envText;
+// Process any entries that start with MONGO_SCO_ or MONGO_CONNECTION_
+function processMongoEntries() {
 
-    let options = self.ssl_defaultOptions;
-
-    // self.useDefaults = (typeof text === 'string') ? self.useDefaultsKeywords[text] : "";
-    //
-    // if (self.useDefaults) {
-    //     if (typeof self[self.useDefaults] === 'object') {
-    //         options = self[self.useDefaults];
-    //     } else if (typeof self[self.useDefaults + '_defaultOptions'] === 'object') {
-    //         options = self[self.useDefaults + '_defaultOptions'];
-    //     } else {
-    //         options = self.ssl_defaultOptions;
-    //     }
-    // } else {
-    //     try {
-    //         options = JSON.parse(text);
-    //     } catch (error) {
-    //         console.log('??? Syntax Error: "%s" parsing env:%s: "%s"',
-    //                     error.message, self.envName, self.envText
-    //         );
-    //         options = self.ssl_defaultOptions;
-    //         console.log('??? Using default ssl connnection options: %s', JSON.stringify(options));
-    //         console.log('??? To use default settings set env:%s to any of the following: ', self.envName)
-    //         console.log('       "%s".', Object.keys(self.useDefaultsKeywords).join(", "));
-    //     }
-    // }
-    return options;
 }
 
-/**
- * Warn the user about any options that should not be set or that are set wrong
- * @param options
- */
-function checkOptions(options) {
-    if (typeof options.ssl !== 'undefined') {
-        console.log('%%%Warning -- do not set ssl=xxx in env:%s.  You must do so in the database uri. %%%', self.envName);
+// Process any env entries that match ourActions
+function processOurActions() {
+    for (const [optionName, optionAction] of Object.entries(ourOptionActions)) {
+        const optionNameLc = optionName.toLowerCase();
+        if (self.mongoEnv[optionNameLc]) {
+            const ourEnv = self.mongoEnv[optionNameLc];
+            optionAction(ourEnv.value, ourEnv.key);
+        }
     }
+}
+
+// Copy all the Mongo_ env variables into mongoenv removing MONGO_xxx prefix
+function loadEnvValues() {
+    const env = process.env;
+    const envPrefix = [];
+    self.envPrefix.forEach(prefix => {
+        envPrefix.push(prefix.toLowerCase())
+    });
+
+    for (let entry in env) {
+        const entrylc = entry.toLowerCase();
+        envPrefix.forEach(prefix => {
+            if (entrylc.startsWith(prefix)) {
+                self.mongoEnv[entrylc.substr(prefix.length)] = {
+                    key: entry,
+                    name: entry.substr(prefix.length),
+                    value: env[entry]
+                };
+            }
+        })
+    }
+}
+
+function setupActions() {
+
+    for (let optionName in self.ourOptions) {
+        ourOptionActions[optionName.toLowerCase()] = self.ourOptions[optionName];
+        optionNames[optionName.toLowerCase()] = optionName;
+    }
+    self.legalOptionNames.forEach(optionName => {
+        optionActions[optionName.toLowerCase()] = foundOption;
+        optionNames[optionName.toLowerCase()] = optionName;
+    });
+
+    self.doNotSetOptions.forEach(optionName => {
+        optionActions[optionName] = doNotSet;
+    })
 }
 
 /**
@@ -212,6 +231,7 @@ function fixupOptions(options) {
  * Read each entry in the options and replace any that start with @
  *  by reading the file with the same name located in the private directory.
  * @param entry
+ * @param name
  * @returns {*}
  */
 function fixupEntry(entry, name) {
@@ -227,45 +247,49 @@ function fixupEntry(entry, name) {
     return entry;
 }
 
-function foundDefault(value) {
-    let defaultName = value + '_defaultOptions';
-    if (typeof self[defaultName] !== 'undefined') {
-        self.options = self[defaultName];
-    } else {
-        Console.log('%s Unrecognized default option: "%s".  Valid values: "%s"',
-                    self.msgPrefix, value, self.defaultOptionNames.join('", "')
-        );
+function foundDefault(value, key) {
+    let defaultName = "";
+    if (typeof self.defaultOptionNames[value] !== undefined) {
+        defaultName = self.defaultOptionNames[value];
+    } else if (typeof self.defaultOptionNames[value.toLowerCase()] !== undefined) {
+        defaultName = self.defaultOptionNames[value.toLowerCase()];
     }
+    if (!defaultName) {
+        error(`${self.msgPrefix} Unrecognized default option:'${value}'  Valid values: "${Object.keys(self.defaultOptionNames).join('", "')}"`);
+        return;
+    }
+
+    self.debugLog("%s setting options to default values for '%s'", self.msgPrefix, defaultName);
+
+    self.options = self[defaultName + '_defaultOptions'];
 }
 
-function allowAny(value) {
-    self.allowAny = !!value;
-    self.debugLog("%s allowAny = '%s'", self.msgPrefix, self.allowAny);
-}
-function setDoDebugLog(value) {
-    self.doDebugLog = !!value;
-    if (self.doDebugLog) {
-        self.debugLog = console.log.bind(self);
-    } else {
-        self.debugLog = self.noop.bind(self);
-    }
-}
 function readFile(value, entry) {
-    return readFilePath(self.filePath + value, entry);
+    return readFilePath(self.filePath + value, value);
 }
 
 function readFilePath(fileName, entry) {
-    self.debugLog("%s reading '%s' -> '%s'", self.msgPrefix, self.entry, fileName);
     let result = null;
+    let readFile = false;
     try {
         if (!self.files[fileName]) {
             self.files[fileName] = fs.readFileSync(fileName);
+            readFile = true;
         }
         result = self.files[fileName];
     } catch (error) {
-        console.log("%s Unable to read '%s' error: %s", self.msgPrefix, entry, fileName);
+        error(
+            `${self.msgPrefix} Unable to read env:'${entry}' error: ${error.message}`
+        );
     }
-    self.debugLog("   --> found file with %d bytes.", result ? result.length : 0);
+    self.debugLog("%s %s '%s' -> '%s' -- %s %d bytes.",
+                  self.msgPrefix,
+                  readFile ? "reading" : "using cached copy",
+                  entry,
+                  fileName,
+                  readFile ? "read" : "reloaded",
+                  result ? result.length : 0
+    );
     return result;     // Only need to read it once
 }
 
@@ -275,8 +299,118 @@ function readEnv(value, entry) {
     if (typeof process.env[result] !== 'undefined') {
         result = process.env[result];
     } else {
-        console.log("%s Unable to read '%s' not defined", self.msgPrefix, entry)
+        error(
+            `${self.msgPrefix} Unable to read env:'${entry}' not defined`
+        );
     }
     self.debugLog("  --> read: '%s'", result);
 }
 
+//*************************  Actions ******************************
+
+function allowAny(value, key) {
+    self.allowAny = !!value;
+    self.debugLog("%s allowAny = '%s'", self.msgPrefix, self.allowAny);
+}
+
+function setDoDebugLog(value, key) {
+    self.doDebugLog = !!value;
+    if (self.doDebugLog) {
+        self.debugLog = console.log.bind(self);
+    } else {
+        self.debugLog = self.noop.bind(self);
+    }
+}
+
+function processOption(optionLc, value, name, key) {
+    self.debugLog("%s process Option '%s'='%s'", self.msgPrefix, name, value);
+    if (ourOptionActions[optionLc]) {
+        ourOptionActions[optionLc](value, name, key);
+        return;
+    }
+    if (optionActions[optionLc]) {
+        optionActions[optionLc](value, name, key);
+        return;
+    }
+    if (self.allowAny) {
+        foundDefault(value, name, key);
+        return;
+    }
+    error(
+        `${self.msgPrefix} invalid option '${name}' found in env:${key}!`
+    );
+}
+
+function setOptions(value, key) {
+    self.debugLog("%s set Options from env:%s '%s'", self.msgPrefix, key, value);
+    // Options are in the form option=value,options=value, ...
+    const options = value.split(',');
+    options.forEach(option => {
+        const optionParts = option.split('=');
+        if (optionParts.length !== 2) {
+            error(
+                `${self.msgPrefix} invalid option '${option}' found in ${key}: '${value}'`
+            );
+        } else {
+            let thisOption = optionParts[0].trim();
+            let thisOptionLc = thisOption.toLowerCase();
+            let thisValue = optionParts[1].trim();
+            processOption(thisOptionLc, thisValue, thisOption, key);
+        }
+    })
+}
+
+function setFilePath(value, key) {
+    const path = value.replace(/^[\s\uFEFF\xA0\"\']+|[\s\uFEFF\xA0\"\']+$/g, '');       // Strip any quotation marks
+    self.debugLog("%s set filePath '%s'", self.msgPrefix, value);
+    self.filePath = value;
+}
+
+function foundOption(value, name, key) {
+    self.debugLog("%s found Option %s='%s'", self.msgPrefix, name, value);
+    self.options[name] = value;
+}
+
+function doNotSet(value, name, key) {
+    warning(
+        [`${self.msgPrefix} Error in env:${key}!`,
+         `${self.msgPaddng}   Do not set '${name}=${value}' here -- set it in the URI! -- Ignoring setting`,
+        ]
+    );
+}
+
+function boxMsg(text, char, minWidth = 40, maxWidth = 120) {
+    if (!Array.isArray(text)) {
+        text = [text];
+    }
+    let maxLen = minWidth;        // Our minimum box size
+    text.forEach(line => {
+        if (line.length > maxLen) {
+            maxLen = line.length;
+        }
+    });
+    maxLen += 6;        // Add in our endings
+    boxSize = maxLen > maxWidth ? maxWidth : maxLen;
+
+    const myText = char + '  ' + text + '  ' + char;
+    console.log(char.repeat(boxSize));
+    console.log(char + ' '.repeat(boxSize - 2) + char);
+    text.forEach(line => {
+        let thisLine = char + '  ' + line;
+        if (thisLine.length < (boxSize - 3)) {
+            thisLine += ' '.repeat((boxSize - 3) - thisLine.length)
+        }
+        thisLine += '  ' + char;
+        console.log(thisLine);
+    });
+    console.log(char + ' '.repeat(boxSize - 2) + char);
+    console.log(char.repeat(boxSize))
+}
+
+function error(text) {
+    boxMsg(text, '?', 40, 120);
+}
+
+function warning(text) {
+    boxMsg(text, '%', 40, 120);
+}
